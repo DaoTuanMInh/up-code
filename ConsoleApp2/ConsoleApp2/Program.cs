403c6e9ef3a2c9af190d12cca1aa70fcb326d7fc
DaoTuanMInh <minhtuan2004.qn@gmail.com>
2024-06-26 12:39:50 +0700

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // An integer
+            int intValue = 1;
+            // A string
+            string stringValue = " Hello " ;
+            // An array of true/false
+            bool[] b = new bool[]
+            {
+            true,
+            false,
+            false
+            };
+            Console.WriteLine();
+        }
+    }
+}
